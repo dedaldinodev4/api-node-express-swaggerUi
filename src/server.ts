e1c024d281e7c98b3b6766d60e3a573926226eb5
Dedaldino Zukk <dedaldino.sebastiao@zukk.com.br>
2021-12-24 19:59:32 +0100

    Insert Readme file

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from "express"
 import { productRouter, categoryRouter, orderRouter } from "./routes"
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocs from './swagger.json'
+import dotenv from 'dotenv'
 
 const app = express();
 
 
+dotenv.config();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
